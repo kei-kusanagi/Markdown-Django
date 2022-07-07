3dc57b844a508eb3c8b575c0fa06171cf519642e
kei_kusanagi_99 <kei_kusanagi@outlook.com>
2022-07-07 14:13:43 -0500

    add models posts, path and views

--- markdownblog/blog/models.py
+++ markdownblog/blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    intro = models.TextField()
+    body = models.TextField()
+    cheated_at = models.DateTimeField(auto_now_add=True)
